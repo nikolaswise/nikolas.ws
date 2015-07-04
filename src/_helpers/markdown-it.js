0e6b669b5b465e6aa8a2f6d539251d7e827ced55
Nikolas Wise <nikolaswise@gmail.com>
2015-07-04 08:30:36 -0700

    awesome markdown

---
+++ src/_helpers/markdown-it.js
@@ -0,0 +1,26 @@
+var md = require('markdown-it')({
+  html: true,
+  linkify: true,
+  typographer: true,
+  quotes: '“”‘’'
+})
+.use(require('markdown-it-highlightjs'))
+.use(require('markdown-it-footnote'))
+.use(require('markdown-it-emoji'))
+.use(require('markdown-it-container'), 'figure')
+
+var typogr = require('typogr');
+
+module.exports = function (site, cb) {
+  site = site.map(function (page) {
+    console.log(page.isMarkdown)
+    if (page.isMarkdown) {
+      page.content = md.render(page.content)
+      page.content = typogr.typogrify(page.content);
+      console.log(page.content)
+
+    }
+    return page
+  })
+  cb(null, site)
+}
