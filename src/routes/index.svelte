9c99def1e3c900d164570ea80534c0b564bec4a1
nikolaswise <nikolaswise@gmail.com>
2022-03-29 21:12:22 -0700

    svelte kit it

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<h1>
+  Nikolas Wise is a web developer, artist, and front-end system architect in Portland, Oregon.
+</h1>
+
+<p>
+  My default answer is "yes". For over 10 years, I have worked on web applications, design systems, and customer facing web experiences with fine artists, design studios, and technology companies.
+</p>
+
+<p>
+  You can reach out to me <a href="mailto:nikolaswise@gmail.com">via email</a>, or elsewhere at <a href="https://github.com/nikolaswise">Github</a> and <a href="https://twitter.com/nikolaswise">Twitter</a>.
+</p>
